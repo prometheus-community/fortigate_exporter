c6a5437e1f5a1da84da04ef19343f0e9e6422e82
Örnfeldt Philip (66140321) <philip.ornfeldt@forsakringskassan.se>
2025-08-06 13:27:43 +0200

    NTP probe added
    Signed-off-by: Örnfeldt Philip (66140321) <philip.ornfeldt@forsakringskassan.se>

---
+++ pkg/probe/system_ntp_status.go
@@ -0,0 +1,99 @@
+// Copyright 2025 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package probe
+
+import (
+	"log"
+	"strconv"
+
+	"github.com/prometheus-community/fortigate_exporter/pkg/http"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+func probeSystemNtpStatus(c http.FortiHTTP, meta *TargetMetadata) ([]prometheus.Metric, bool) {
+	var (
+		ntpExpires = prometheus.NewDesc(
+			"fortigate_system_ntp_expires",
+			"NTP expire time, in seconds",
+			[]string{"ip", "server", "reachable", "selected", "version"}, nil,
+		)
+		ntpStratum = prometheus.NewDesc(
+			"fortigate_system_ntp_stratum",
+			"NTP stratum value",
+			[]string{"ip", "server", "reachable", "selected", "version"}, nil,
+		)
+		ntpRefTime = prometheus.NewDesc(
+			"fortigate_system_ntp_reftime",
+			"NTP reftime in epoch seconds",
+			[]string{"ip", "server", "reachable", "selected", "version"}, nil,
+		)
+		ntpOffset = prometheus.NewDesc(
+			"fortigate_system_ntp_offset",
+			"NTP combined offset, in milliseconds",
+			[]string{"ip", "server", "reachable", "selected", "version"}, nil,
+		)
+		ntpDelay = prometheus.NewDesc(
+			"fortigate_system_ntp_delay",
+			"NTP round trip delay, in milliseconds",
+			[]string{"ip", "server", "reachable", "selected", "version"}, nil,
+		)
+		ntpDispersion = prometheus.NewDesc(
+			"fortigate_system_ntp_dispersion",
+			"NTP dispersion to primary clock, in milliseconds",
+			[]string{"ip", "server", "reachable", "selected", "version"}, nil,
+		)
+		ntpPeerDispersion = prometheus.NewDesc(
+			"fortigate_system_ntp_dispersion_peer",
+			"NTP peer dispersion, in milliseconds",
+			[]string{"ip", "server", "reachable", "selected", "version"}, nil,
+		)
+	)
+
+	type SystemNtpStatus struct {
+		Ip             string  `json:"ip"`
+		Server         string  `json:"server"`
+		Reachable      bool    `json:"reachable"`
+		Expires        int     `json:"expires"`
+		Selected       bool    `json:"selected"`
+		Version        int     `json:"version"`
+		Stratum        int     `json:"stratum"`
+		Reftime        int     `json:"reftime"`
+		Offset         float64 `json:"offset"`
+		Delay          float64 `json:"delay"`
+		Dispersion     float64 `json:"dispersion"`
+		PeerDispersion int     `json:"peer_dispersion"`
+	}
+
+	type SystemNtpStatusResult struct {
+		Results []SystemNtpStatus `json:"results"`
+	}
+
+	var res SystemNtpStatusResult
+	if err := c.Get("api/v2/monitor/system/ntp/status","",&res); err != nil {
+		log.Printf("Error: %v", err)
+		return nil, false
+	}
+
+	m := []prometheus.Metric{}
+	for _, r := range res.Results {
+		m = append(m, prometheus.MustNewConstMetric(ntpExpires, prometheus.GaugeValue, float64(r.Expires), r.Ip, r.Server, strconv.FormatBool(r.Reachable), strconv.FormatBool(r.Reachable), strconv.Itoa(r.Version)))
+		m = append(m, prometheus.MustNewConstMetric(ntpStratum, prometheus.GaugeValue, float64(r.Stratum), r.Ip, r.Server, strconv.FormatBool(r.Reachable), strconv.FormatBool(r.Reachable), strconv.Itoa(r.Version)))
+		m = append(m, prometheus.MustNewConstMetric(ntpRefTime, prometheus.CounterValue, float64(r.Reftime), r.Ip, r.Server, strconv.FormatBool(r.Reachable), strconv.FormatBool(r.Reachable), strconv.Itoa(r.Version)))
+		m = append(m, prometheus.MustNewConstMetric(ntpOffset, prometheus.GaugeValue, float64(r.Offset), r.Ip, r.Server, strconv.FormatBool(r.Reachable), strconv.FormatBool(r.Reachable), strconv.Itoa(r.Version)))
+		m = append(m, prometheus.MustNewConstMetric(ntpDelay, prometheus.GaugeValue, float64(r.Delay), r.Ip, r.Server, strconv.FormatBool(r.Reachable), strconv.FormatBool(r.Reachable), strconv.Itoa(r.Version)))
+		m = append(m, prometheus.MustNewConstMetric(ntpDispersion, prometheus.GaugeValue, float64(r.Dispersion), r.Ip, r.Server, strconv.FormatBool(r.Reachable), strconv.FormatBool(r.Reachable), strconv.Itoa(r.Version)))
+		m = append(m, prometheus.MustNewConstMetric(ntpPeerDispersion, prometheus.GaugeValue, float64(r.PeerDispersion), r.Ip, r.Server, strconv.FormatBool(r.Reachable), strconv.FormatBool(r.Reachable), strconv.Itoa(r.Version)))
+	}
+	return m, true
+}
---
+++ pkg/probe/system_ntp_status_test.go
@@ -0,0 +1,72 @@
+// Copyright 2025 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package probe
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/testutil"
+)
+
+func TestSystemNtpStatus(t *testing.T) {
+	c := newFakeClient()
+	c.prepare("api/v2/monitor/system/ntp/status", "testdata/system-ntp-status.jsonnet")
+	r := prometheus.NewPedanticRegistry()
+	if !testProbe(probeSystemNtpStatus, c, r) {
+		t.Errorf("probeSystemNtpStatus() returned non-success")
+	}
+
+	em := `# HELP fortigate_system_ntp_delay NTP round trip delay, in milliseconds
+	# TYPE fortigate_system_ntp_delay gauge
+	fortigate_system_ntp_delay{ip="127.0.0.1",reachable="true",selected="true",server="HA-TEST",version="1035"} 324
+	fortigate_system_ntp_delay{ip="127.0.0.2",reachable="false",selected="false",server="HA-CAP",version="1035"} 324
+	fortigate_system_ntp_delay{ip="127.0.0.3",reachable="true",selected="true",server="HA-CODE",version="1035"} 324
+	# HELP fortigate_system_ntp_dispersion NTP dispersion to primary clock, in milliseconds
+	# TYPE fortigate_system_ntp_dispersion gauge
+	fortigate_system_ntp_dispersion{ip="127.0.0.1",reachable="true",selected="true",server="HA-TEST",version="1035"} 342
+	fortigate_system_ntp_dispersion{ip="127.0.0.2",reachable="false",selected="false",server="HA-CAP",version="1035"} 342
+	fortigate_system_ntp_dispersion{ip="127.0.0.3",reachable="true",selected="true",server="HA-CODE",version="1035"} 342
+	# HELP fortigate_system_ntp_dispersion_peer NTP peer dispersion, in milliseconds
+	# TYPE fortigate_system_ntp_dispersion_peer gauge
+	fortigate_system_ntp_dispersion_peer{ip="127.0.0.1",reachable="true",selected="true",server="HA-TEST",version="1035"} 123
+	fortigate_system_ntp_dispersion_peer{ip="127.0.0.2",reachable="false",selected="false",server="HA-CAP",version="1035"} 123
+	fortigate_system_ntp_dispersion_peer{ip="127.0.0.3",reachable="true",selected="true",server="HA-CODE",version="1035"} 123
+	# HELP fortigate_system_ntp_expires NTP expire time, in seconds
+	# TYPE fortigate_system_ntp_expires gauge
+	fortigate_system_ntp_expires{ip="127.0.0.1",reachable="true",selected="true",server="HA-TEST",version="1035"} 145438
+	fortigate_system_ntp_expires{ip="127.0.0.2",reachable="false",selected="false",server="HA-CAP",version="1035"} 124438
+	fortigate_system_ntp_expires{ip="127.0.0.3",reachable="true",selected="true",server="HA-CODE",version="1035"} 245438
+	# HELP fortigate_system_ntp_offset NTP combined offset, in milliseconds
+	# TYPE fortigate_system_ntp_offset gauge
+	fortigate_system_ntp_offset{ip="127.0.0.1",reachable="true",selected="true",server="HA-TEST",version="1035"} 5482
+	fortigate_system_ntp_offset{ip="127.0.0.2",reachable="false",selected="false",server="HA-CAP",version="1035"} 5482
+	fortigate_system_ntp_offset{ip="127.0.0.3",reachable="true",selected="true",server="HA-CODE",version="1035"} 5482
+	# HELP fortigate_system_ntp_reftime NTP reftime in epoch seconds
+	# TYPE fortigate_system_ntp_reftime counter
+	fortigate_system_ntp_reftime{ip="127.0.0.1",reachable="true",selected="true",server="HA-TEST",version="1035"} 85742
+	fortigate_system_ntp_reftime{ip="127.0.0.2",reachable="false",selected="false",server="HA-CAP",version="1035"} 85742
+	fortigate_system_ntp_reftime{ip="127.0.0.3",reachable="true",selected="true",server="HA-CODE",version="1035"} 85742
+	# HELP fortigate_system_ntp_stratum NTP stratum value
+	# TYPE fortigate_system_ntp_stratum gauge
+	fortigate_system_ntp_stratum{ip="127.0.0.1",reachable="true",selected="true",server="HA-TEST",version="1035"} 3
+	fortigate_system_ntp_stratum{ip="127.0.0.2",reachable="false",selected="false",server="HA-CAP",version="1035"} 3
+	fortigate_system_ntp_stratum{ip="127.0.0.3",reachable="true",selected="true",server="HA-CODE",version="1035"} 3
+	`
+
+	if err := testutil.CollectAndCompare(r, strings.NewReader(em)); err != nil {
+		t.Fatalf("metric compare: err %v", err)
+	}
+}
